57f37c6ee0d49b0e8b3fa3565e50a6b7c06b253e
Tyler Hall <tylerwhall@gmail.com>
2010-01-25 23:06:51 -0500

    CECS: Add CECS 525 minimal 68000 device
    Also add the 6850 ACIA

---
+++ hw/acia_6850.c
@@ -0,0 +1,172 @@
+/*
+ * 6850 ACIA
+ *
+ * Copyright (c) 2010 Tyler Hall.
+ *
+ * This code is licenced under the GPL
+ */
+
+#include <inttypes.h>
+
+#include "hw.h"
+#include "sysbus.h"
+
+#define ACIA_CONTROL 0x1
+#define ACIA_DATA 0x3
+
+#define STATUS_IRQ      (1 << 7)
+#define STATUS_PE       (1 << 6)
+#define STATUS_OVRN     (1 << 5)
+#define STATUS_FE       (1 << 4)
+#define STATUS_CTS      (1 << 3)
+#define STATUS_DCD      (1 << 2)
+#define STATUS_TDRE     (1 << 1)
+#define STATUS_RDRF     (1 << 0)
+
+#define CONTROL_RXIRQ   (1 << 7)
+
+struct acia_6850 {
+    SysBusDevice busdev;
+    CharDriverState *chr;
+    qemu_irq irq;
+    uint8_t status;
+    uint8_t control;
+    char buf;
+};
+
+static void acia_update_irq(struct acia_6850 *s)
+{
+    int rx_irq = s->status & STATUS_RDRF && s->control & CONTROL_RXIRQ;
+
+    if (rx_irq) {
+        s->status &= ~STATUS_IRQ;
+        qemu_irq_lower(s->irq);
+    } else {
+        s->status |= STATUS_IRQ;
+        qemu_irq_raise(s->irq);
+    }
+}
+
+static void acia_rx(void *opaque, const uint8_t *buf, int size)
+{
+    struct acia_6850 *s = opaque;
+
+    if (size > 1) {
+        printf("Uart dropped character\n");
+    } else if (size == 1) {
+        s->buf = *buf;
+        s->status |= STATUS_RDRF;
+    }
+    acia_update_irq(s);
+}
+
+static int acia_can_rx(void *opaque)
+{
+    struct acia_6850 *s = opaque;
+
+    if (s->status & STATUS_RDRF) {
+        return 0;
+    } else {
+        return 1;
+    }
+}
+
+static void acia_event(void *opaque, int event)
+{
+}
+
+static uint32_t acia_6850_mem_readb(void *opaque, target_phys_addr_t addr)
+{
+    struct acia_6850 *s = opaque;
+    uint32_t value = 0;
+
+    switch (addr) {
+        case ACIA_CONTROL:
+            value = s->status;
+            value |= STATUS_TDRE;
+            break;
+        case ACIA_DATA:
+            value = s->buf;
+            s->status &= ~STATUS_RDRF;
+            acia_update_irq(s);
+            break;
+        default:
+            printf("Unknown acia mem read %"PRIx32"\n ", (uint32_t)addr);
+    };
+    return value;
+}
+
+static uint32_t acia_6850_mem_readf(void *opaque, target_phys_addr_t addr)
+{
+    printf("ACIA read more than one byte\n");
+    exit(1);
+    return 0;
+}
+
+static void acia_6850_mem_writeb(void *opaque, target_phys_addr_t addr, uint32_t val)
+{
+    struct acia_6850 *s = opaque;
+
+    switch (addr) {
+        case ACIA_CONTROL:
+            s->control = val;
+            acia_update_irq(s);
+            break;
+        case ACIA_DATA:
+            qemu_chr_write(s->chr, (uint8_t *)&val, 1);
+            break;
+        default:
+            printf("Unknown acia mem write %"PRIx32"\n", (uint32_t)addr);
+    };
+}
+
+static void acia_6850_mem_writef(void *opaque, target_phys_addr_t addr, uint32_t val)
+{
+    printf("ACIA wrote more than one byte\n");
+    exit(1);
+}
+
+static CPUReadMemoryFunc * const acia_6850_mem_read[] = {
+    acia_6850_mem_readb,
+    acia_6850_mem_readf,
+    acia_6850_mem_readf,
+};
+
+static CPUWriteMemoryFunc * const acia_6850_mem_write[] = {
+    acia_6850_mem_writeb,
+    acia_6850_mem_writef,
+    acia_6850_mem_writef,
+};
+
+static int acia_6850_init(SysBusDevice *dev)
+{
+    struct acia_6850 *s = (struct acia_6850 *) dev;
+    int regs;
+
+    /* Initialize */
+    s->status = s->control = s->buf = 0;
+    s->irq = NULL;
+
+    /* Set up memory-mapped io */
+    regs = cpu_register_io_memory(acia_6850_mem_read,
+                                  acia_6850_mem_write, s);
+    sysbus_init_mmio(dev, 4, regs);
+
+    /* Request an irq line */
+    sysbus_init_irq(dev, &s->irq);
+
+    /* Grab a qemu serial device */
+    s->chr = qdev_init_chardev(&dev->qdev);
+    if (s->chr)
+        qemu_chr_add_handlers(s->chr, acia_can_rx, acia_rx, acia_event, s);
+
+    return 0;
+}
+
+static void acia_6850_register_devices(void)
+{
+    sysbus_register_dev("acia-6850", sizeof(struct acia_6850),
+                        acia_6850_init);
+}
+
+device_init(acia_6850_register_devices);
---
+++ hw/cecs_m68k.c
@@ -0,0 +1,205 @@
+/*
+ * 68K minimal computer.
+ * Used for CECS 525 at the University of Louisville
+ *
+ * Copyright (c) 2010 Tyler Hall.
+ *
+ * This code is licenced under the GPL
+ */
+
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <inttypes.h>
+
+#include "hw.h"
+#include "sysemu.h"
+#include "boards.h"
+#include "loader.h"
+#include "block.h"
+#include "block_int.h"
+#include "sysbus.h"
+
+#define ROM_ADDR 0x0000
+#define ROM_SIZE 0x2000 //8k
+
+#define RAM_ADDR 0x4000
+#define RAM_SIZE 0x1000 //4k
+
+typedef enum {
+    SREC_IDLE,
+    SREC_TYPE,
+    SREC_COUNT,
+    SREC_ADDR,
+    SREC_DATA,
+} srec_state;
+
+static inline char xatoi(char val)
+{
+    switch (val) {
+        case '0' ... '9':
+            val -= '0';
+            break;
+        case 'a' ... 'f':
+            val = 10 + val - 'a';
+            break;
+        case 'A' ... 'F':
+            val = 10 + val - 'A';
+            break;
+    }
+    return val;
+}
+
+static void srec_byte(uint8_t input)
+{
+    static srec_state state;
+    static int first_nibble;
+    static uint8_t byte;
+    static int addr_len;
+    static uint32_t addr;
+    static int len;
+    static int buf_count;
+    static uint8_t buf[256];
+
+    if (input == 'S') {
+        state = SREC_TYPE;
+        return;
+    }
+
+    if (state >= SREC_COUNT) {
+        if (first_nibble) {
+            byte = xatoi(input) << 4;
+            first_nibble = 0;
+            return;
+        } else {
+            byte |= xatoi(input);
+            first_nibble = 1;
+        }
+    }
+
+    switch (state) {
+        case SREC_IDLE:
+            break;
+        case SREC_TYPE: {
+            int type;
+
+            type = input - '0';
+            if (type >= 1 && type <= 3) {
+                addr_len = type + 1;
+                state = SREC_COUNT;
+                first_nibble = 1;
+                addr = 0;
+            } else {
+                state = SREC_IDLE;
+            }
+            break;
+        }
+        case SREC_COUNT:
+            len = byte - 1; //Ignore the checksum at the end
+            len -= addr_len; //len is only the length of the actual data
+            state = SREC_ADDR;
+            buf_count = 0;
+            break;
+        case SREC_ADDR:
+            addr <<= 8;
+            addr |= byte;
+            if (--addr_len == 0)
+                state = SREC_DATA;
+            break;
+        case SREC_DATA:
+            buf[buf_count++] = byte;
+            if (buf_count >= sizeof(buf)) {
+                printf("SREC Line buffer exceeded\n");
+                state = SREC_IDLE;
+            } else if (buf_count >= len) {
+                cpu_physical_memory_write_rom(addr, buf, len);
+                printf("Line read. Addr = 0x%x,Len = %d\n", addr, len);
+                state = SREC_IDLE;
+            }
+            break;
+    }
+}
+
+static void srec_input(uint8_t *buf, int size)
+{
+    int i;
+
+    for (i = 0; i < size; i++)
+        srec_byte(buf[i]);
+}
+
+static int copy_rom(const char *filename)
+{
+    int fd;
+    uint8_t buf[256];
+    int count;
+
+    fd = open(filename, O_RDONLY);
+    if (fd < 0)
+        return -1;
+    do {
+        count = read(fd, buf, sizeof(buf));
+        srec_input(buf, count);
+    } while (count);
+
+    return 0;
+}
+
+static void cecs_m68k_init(ram_addr_t ram_size,
+                     const char *boot_device,
+                     const char *kernel_filename, const char *kernel_cmdline,
+                     const char *initrd_filename, const char *cpu_model)
+{
+    CPUState *env;
+    target_phys_addr_t entry;
+
+    if (!cpu_model)
+        cpu_model = "m68000";
+    env = cpu_init(cpu_model);
+    if (!env) {
+        fprintf(stderr, "Unable to find m68k CPU definition\n");
+        exit(1);
+    }
+
+    env->vbr = 0;
+
+    /* Create memory */
+    cpu_register_physical_memory(ROM_ADDR, ROM_SIZE,
+        qemu_ram_alloc(ROM_SIZE) | IO_MEM_ROM);
+
+    cpu_register_physical_memory(RAM_ADDR, RAM_SIZE,
+        qemu_ram_alloc(RAM_SIZE) | IO_MEM_RAM);
+
+    if (!kernel_filename) {
+        printf("Must specify rom as kernel\n");
+        exit(1);
+    }
+
+    if(copy_rom(kernel_filename)) {
+        printf("Failed to load rom\n");
+        exit(1);
+    }
+
+    sysbus_create_simple("acia-6850", 0x8000, NULL);
+
+    /* Read reset vector */
+    cpu_physical_memory_read(0x4, (uint8_t *)&entry, sizeof(entry));
+    entry = tswap32(entry);
+    printf("Reset vector = 0x%"PRIx32"\n", (uint32_t) entry);
+    env->pc = entry;
+}
+
+static QEMUMachine cecs_m68k_machine = {
+    .name = "cecs",
+    .desc = "CECS 525 minimal computer",
+    .init = cecs_m68k_init,
+};
+
+static void cecs_m68k_machine_init(void)
+{
+    qemu_register_machine(&cecs_m68k_machine);
+}
+
+machine_init(cecs_m68k_machine_init);
